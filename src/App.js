0d0732c3a5bfca621d0e9cbe3d40dfca9d631f2c
Aktilek <nasirovaktilek@gmail.com>
2022-06-06 11:31:46 +0600

    home page carousel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MainRoutes from "./MainRoutes";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const App = () => {
   return (
     <BrowserRouter>
