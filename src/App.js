03a4c9cb05b05ed66cae43e3f18fd73a2118e205
Aktilek <nasirovaktilek@gmail.com>
2022-06-06 18:46:25 +0600

    addproduct, db.json, productContext

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter } from "react-router-dom";
 import PrimarySearchAppBar from "./Components/Navbar/Navbar";
 import MainRoutes from "./MainRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
+import ProductContextProvider from "./Context/ProductContext";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <PrimarySearchAppBar />
-      <MainRoutes />
-      {/* <Footer /> */}
+      <ProductContextProvider>
+        <PrimarySearchAppBar />
+        <MainRoutes />
+      </ProductContextProvider>
     </BrowserRouter>
   );
 };
