97ca54c62db4a254e28b3293430537e53af18267
Kim <kim@MacBook-AirEkaterina.local>
2022-06-06 11:45:45 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import PrimarySearchAppBar from "./Components/Navbar/Navbar";
 import MainRoutes from "./MainRoutes";
+
 const App = () => {
   return (
     <BrowserRouter>
+      <PrimarySearchAppBar />
       <MainRoutes />
     </BrowserRouter>
   );
