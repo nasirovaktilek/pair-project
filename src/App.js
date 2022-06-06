1b879aee6d096e94549e7a181ca9854a2354bc0f
Aktilek <nasirovaktilek@gmail.com>
2022-06-06 19:15:22 +0600

    checkout

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PrimarySearchAppBar from "./Components/Navbar/Navbar";
 import MainRoutes from "./MainRoutes";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ProductContextProvider from "./Context/ProductContext";
+import AuthContextProvider from "./Context/AuthContext";
 
 const App = () => {
   return (
